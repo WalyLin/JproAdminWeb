5a171b4b120b5361c71051f97362645be6368042
林振雷 <308698774@qq.com>
2024-12-15 22:24:27 +0800

    发布

--- dist/assets/index-BgrI5ePX.js
+++
@@ -1,1 +0,0 @@
-import{Z as r,c,e as s,h as o,o as e}from"./index-zqoWALrn.js";import a from"./auth-BB6fDpfK.js";import n from"./docMain-DG6XihLf.js";import"./doc-BoXLCq3d.js";const m={class:"h-full"},d={__name:"index",setup(_){const t=r();return(i,p)=>(e(),c("div",m,[s(t).auth?(e(),o(n,{key:0})):(e(),o(a,{key:1}))]))}};export{d as default};
